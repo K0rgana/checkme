f9fab84f3e6dffbf22b201da659686764425cb24
AlissonThyago <alissonthiagoamorim@gmail.com>
2019-10-28 20:15:58 -0300

    GH-4 login e cadastro

--- checkme/database/migrations/2014_10_12_000000_create_users_table.php
+++ checkme/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('tipo_sanguineo', ['O−', 'O+', 'A-', 'A+', 'B-', 'B+', 'AB-', 'AB+']);
+            $table->enum('tipo', ['medico', 'usuario']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
